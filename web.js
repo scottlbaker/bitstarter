70f3def7bfeeb9a92f84160ed6cf0c34be1e37eb
Scott Baker <scott.l.baker@intel.com>
2013-07-08 00:19:59 +0000

    added fs module

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
 var data = fs.readFileSync("index.html", "utf8");
 
 app.get('/', function(request, response) {
